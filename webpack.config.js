fd1d0369f1ef1e24097a13e1f9274df532cc8a94
Ammar <muhammad.ammarhassan@1ststop.co.uk>
2020-09-16 22:19:11 +0100

    working with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,56 @@
 const path = require("path");
-
 module.exports = {
-    entry : "./src/index.js",
+    entry: "./src/index.js",
     output: {
+        // globalObject: "this",
         filename: "main.js",
-        path: path.resolve(__dirname, "dist")
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        port: 900
+        path: path.resolve(__dirname, "dist"),
     },
     module: {
-        rules : [
-            // {
-            //     test: /\.css$/,
-            //     use: [
-            //         {loader: 'style-loader'},
-            //         {loader: 'css-loader'}
-            //     ]
-            // },
-        ]
-    }
-
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+    //         {
+    //             test: /\.html$/,
+    //             use: [
+    //                 {
+    //                     loader: "html-loader",
+    //                     options: {minimize : true}
+    //                 }
+    //             ]
+    //         },
+    //         {
+    //             test: /\.(png|svg|jpg|gif)/,
+    //             use: [
+    //                 'file-loader'
+    //             ]
+    //         },
+    //         {
+    //             test: /\.scss$/,
+    //             use : [
+    //                 "css-loader",
+    //                 "sass-loader"
+    //             ]
+    //         }
+    //     ]
+    // },
+    // plugins: [
+    //   new HtmlWebPackPlugin({
+    //       template: "./src/index.html",
+    //       filename: "./index.html"
+    //   }),
+    //   new MiniCssExtractPlugin({
+    //       filename: "[name].css",
+    //       chunkFilename: "[id].css"
+    //   })  
+    ]
+}
 }
