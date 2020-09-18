079f46be08e2dab5556b14a26e16cf9a182c3534
Ammar <muhammad.ammarhassan@1ststop.co.uk>
2020-09-18 09:29:08 +0100

    latest

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,17 +1,50 @@
-const productName = [];
-document.addEventListener("DOMContentLoaded", function () {
-  fetch("./products.json")
-    .then((res) => res.json())
-    .then((data) => {
-      Highcharts.chart("container", {
-        title: {
-          text: "Daily Sales Overview",
-        },
-        series: [
-          {
-            data: data,
-          },
-        ],
-      });
-    });
-});
+  
+  // Data
+  function getData() {
+      fetch('./products.json').then(function(response) {
+        return response.json()
+      }).then(function(data) {
+        chart.series[0].addPoint(data[0].qty),
+        chart.series[1].addPoint(data[1].qty)
+      })
+  }
+
+  // Chart
+var options = {
+    chart: {
+      type: 'column',
+      events: {
+          load: getData
+      }
+    },
+    title: {
+      text: 'Sales Dashboard'
+    },
+    subtitle: {
+        text: `Total Sale ${}`
+    },
+    xAxis: {
+     // type: 'datetime',
+     
+    },
+    yAxis: {
+      title: {
+        text: 'Price (USD)'
+      }
+    },
+    legend: {
+      enabled: false
+    },
+    exporting: {
+      enabled: false
+    },
+    series: [{
+      name: 'Jeans',
+      data: []
+    },
+    {
+        name: 'Tees',
+        data: []
+      }]
+  };
+  var chart = Highcharts.chart('container', options)
